933bbd7386900666cb05b384b2f9ebfa2d40cdc7
cchouffon <58453159+cchouffon@users.noreply.github.com>
2020-01-30 10:12:52 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      
+ <title><?php echo "Assignment 7"; ?></title>
+  <meta charset="utf-8"> 
+</head>
+<body>
+
+<?php 
+$meatloaf = 'Rice';
+echo $meatloaf;
+echo "<br>";
+$my_Num = 2; 
+echo $my_Num;
+echo "<br>";
+$txt = 'Red Rocks Park';
+echo $txt;
+echo "<br>";
+$chickenhawk = 'Rabbit';
+echo $chickenhawk;
+?>
+
+
+
+<h1>Good morning, Nebraska!</h1>
+<div>Last night I ate <?php echo $meatloaf ?>.</div>
+<div>While it was good, it wasn't perfect. I'd give it <?php echo $my_Num ?> start out of 4.</div>
+<div>Tomorrow I plan to go to the <?php echo $txt ?>.</div>
+<div> My favorite animal is the <?php echo $chickenhawk ?>.</div>
+</body>
+</html>
